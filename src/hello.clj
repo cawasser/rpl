db6391c0b0e19a33f51cb9667fbb9f5762c0aab2
Chris Wasser <cawasser@mac.com>
2019-07-29 00:30:58 -0400

    weekend experimentation

--- src/hello.clj
+++ src/hello.clj
@@ -1,53 +1,70 @@
 (ns hello
   (:require [clj-time.core :as t]
             [clj-time.format :as f]
-            [crux.api :as crux]))
+            [crux.api :as crux]
+            [injest :as i]
+            [clojure.pprint :as pp]))
 
 (def ^crux.api.ICruxAPI node
-  (crux/start-standalone-node {:kv-backend "crux.kv.memdb.MemKv"
-                               :db-dir "data/db-dir-1"
+  (crux/start-standalone-node {:kv-backend    "crux.kv.memdb.MemKv"
+                               :db-dir        "data/db-dir-1"
                                :event-log-dir "data/eventlog-1"}))
+(defn db [] (crux/db node))
+
 (use 'hello)
 
-(crux/submit-tx
-  node
-  ; tx type
-  [[:crux.tx/put
-
-    {:crux.db/id :ids.people/Charles  ; mandatory id for a document in Crux
-     :person/name "Charles"
-     ; age 40 at 1740
-     :person/born #inst "1700-05-18"
-     :person/location :ids.places/rarities-shop
-     :person/str  40
-     :person/int  40
-     :person/dex  40
-     :person/hp   40
-     :person/gold 10000}
-
-    #inst "1700-05-18"]])
-
-
-(crux/submit-tx
-  node
-  [[:crux.tx/put
-    {:crux.db/id :dbpedia.resource/Pablo-Picasso ; id
-     :name "Pablo"
-     :last-name "Picasso"}
-    #inst "2018-05-18T09:20:27.966-00:00"]])
-
-
-(def db (crux/db node))
-
-(crux/q db
-        '{:find [e]
-          :where [[e :name "Pablo"]]})
-
-
-(crux/q db '{:find [e]
-             :where [[e :person/str]]})
+(i/people node)
+(i/artifacts node)
+(i/places node)
 
 
 
-(defn -main []
-  (println "Hello world, the time is" (time-str (t/now))))
+(crux/q (db) '{:find  [?e]
+               :where [[?e :person/str]]})
+
+(pp/print-table
+  (for [e (crux/q (db) '{:find  [?e]
+                         :where [[?e :person/str]]})]
+    (crux/entity (db) (first e))))
+
+(crux/q (db) '{:find  [?name]
+               :where [[_ :place/title ?name]]})
+
+
+
+; FYI - print-table is 20 lines of code
+(pp/print-table [(crux/entity (db) :ids.people/Charles)])
+
+
+
+(crux/q (db) '{:find  [?id]
+               :where [[?id :person/name ?name]]
+               :args  [{?name "Charles"}
+                       {?name "Joe"}]})
+
+
+(crux/q (db) '{:find  [?id]
+               :where [[(re-find #"J" ?name)]
+                       [?id :person/name ?name]]})
+
+
+(defn reg-find [db r]
+      (crux/q db {:find  ['?id]
+                  :where [[(re-find r '?name)]
+                          ['?id :person/name '?name]]
+                  :args [('regex r)]}))
+(reg-find (db) #"J")
+
+
+(defn age-find [db a]
+  (crux/q db {:find  '[?age]
+              :where '[[(>= ?age 21)]]
+              :args (into []
+                          (for [i a]
+                            {'?age i}))}))
+(age-find (db) [22 23 20])
+
+
+
+
+
--- src/injest.clj
+++ src/injest.clj
@@ -1,1 +1,106 @@
-(ns injest)
+(ns injest
+  (:require [clj-time.core :as t]
+            [clj-time.format :as f]
+            [crux.api :as crux]))
+
+
+(defn people [node]
+  (crux/submit-tx
+    node
+    [; characters
+     [:crux.tx/put
+      {:crux.db/id      :ids.people/Charles  ; mandatory id for a document in Crux
+       :person/name     "Charles"
+       ; age 40 at 1740
+       :person/born     #inst "1700-05-18"
+       :person/location :ids.places/rarities-shop
+       :person/str      40
+       :person/int      40
+       :person/dex      40
+       :person/hp       40
+       :person/gold     10000}
+
+      #inst "1700-05-18"]
+     [:crux.tx/put
+      {:crux.db/id :ids.people/Mary
+       :person/name "Mary"
+       ; age  30
+       :person/born #inst "1710-05-18"
+       :person/location :ids.places/carribean
+       :person/str  40
+       :person/int  50
+       :person/dex  50
+       :person/hp   50}
+      #inst "1710-05-18"]
+     [:crux.tx/put
+      {:crux.db/id :ids.people/Joe
+       :person/name "Joe"
+       ; age  25
+       :person/born #inst "1715-05-18"
+       :person/location :ids.places/city
+       :person/str  39
+       :person/int  40
+       :person/dex  60
+       :person/hp   60
+       :person/gold 70}
+      #inst "1715-05-18"]]))
+
+(defn artifacts [node]
+  (crux/submit-tx
+    node
+    [; artefacts
+     ; In our tale there is a Cozy Mug...
+     [:crux.tx/put
+      {:crux.db/id :ids.artefacts/cozy-mug
+       :artefact/title "A Rather Cozy Mug"
+       :artefact.perks/int 3}
+      #inst "1625-05-18"]
+
+     ; ...some regular magic beans...
+     [:crux.tx/put
+      {:crux.db/id :ids.artefacts/forbidden-beans
+       :artefact/title "Magic beans"
+       :artefact.perks/int 30
+       :artefact.perks/hp -20}
+
+      #inst "1500-05-18"]
+     ; ...a used pirate sword...
+     [:crux.tx/put
+      {:crux.db/id :ids.artefacts/pirate-sword
+       :artefact/title "A used sword"}
+      #inst "1710-05-18"]
+     ; ...a flintlock pistol...
+     [:crux.tx/put
+      {:crux.db/id :ids.artefacts/flintlock-pistol
+       :artefact/title "Flintlock pistol"}
+      #inst "1710-05-18"]
+     ; ...a mysterious key...
+     [:crux.tx/put
+      {:crux.db/id :ids.artefacts/unknown-key
+       :artefact/title "Key from an unknown door"}
+      #inst "1700-05-18"]
+     ; ...and a personal computing device from the wrong century.
+     [:crux.tx/put
+      {:crux.db/id :ids.artefacts/laptop
+       :artefact/title "A Tell DPS Laptop (what?)"}
+      #inst "2016-05-18"]]))
+
+
+  ; places
+(defn places [node]
+  (crux/submit-tx
+    node
+    [[:crux.tx/put
+      {:crux.db/id :ids.places/continent
+       :place/title "Ah The Continent"}
+      #inst "1000-01-01"]
+     [:crux.tx/put
+      {:crux.db/id :ids.places/carribean
+       :place/title "Ah The Good Ol Carribean Sea"
+       :place/location :ids.places/carribean}
+      #inst "1000-01-01"]
+     [:crux.tx/put
+      {:crux.db/id :ids.places/coconut-island
+       :place/title "Coconut Island"
+       :place/location :ids.places/carribean}
+      #inst "1000-01-01"]]))
