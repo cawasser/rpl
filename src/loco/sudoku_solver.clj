94b58928abfc4ffc9be41171fc1c5793507b26b3
Chris Wasser <cawasser@mac.com>
2022-11-07 09:45:09 -0500

    - added a broken version og "world's hardest Sudoku"

--- src/loco/sudoku_solver.clj
+++ src/loco/sudoku_solver.clj
@@ -19,6 +19,16 @@
    [0 0 8 5 0 0 0 1 0]
    [0 9 0 0 0 0 4 0 0]])
 
+(def BROKEN-worlds-hardest-puzzle
+  [[8 0 0 8 0 0 0 0 0]
+   [0 0 3 6 0 0 0 0 0]
+   [0 7 0 0 9 0 2 0 0]
+   [0 5 0 0 0 7 0 0 0]
+   [0 0 0 0 4 5 7 0 0]
+   [0 0 0 1 0 0 0 3 0]
+   [0 0 1 0 0 0 0 6 8]
+   [0 0 8 5 0 0 0 1 0]
+   [0 9 0 0 0 0 4 0 0]])
 ; see https://imprimesudoku.blogspot.com/2014/07/medium-sudoku-21-30.html
 (def puzzle-1
   [[0 0 8 0 0 0 5 0 0]
