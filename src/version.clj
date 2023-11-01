fe5f732c9f17b3f6206c1600953616386a1e3c13
Chris Wasser <cawasser@mac.com>
2023-11-01 16:06:18 -0400

    Bump rpl to 0.0.2 and spit metadata

--- src/version.clj
+++ src/version.clj
@@ -3,6 +3,6 @@
 
 (def module-name "rpl")
 (def path ".")
-(def version "0.0.1-DIRTY")
-(def tag "v0.0.1-DIRTY")
-(def generated-at "2023-11-01T20:00:18Z")
+(def version "0.0.2")
+(def tag "v0.0.2")
+(def generated-at "2023-11-01T20:06:18Z")
